fix causal mask

gpt2.py
@@ -5,7 +5,7 @@ from typing import List, Dict, Any
 
 def lm_loss(params, inputs, n_head) -> float:
     x, y = inputs[:-1], inputs[1:]
-    output = gpt2(x, ** params, n_head=n_head)
+    output = gpt2(x, **params, n_head=n_head)
     loss = jnp.mean(-jnp.log(output[y]))
     return loss
 
@@ -23,9 +23,7 @@ def attention(q: jnp.array, k: jnp.array, v: jnp.array, mask: jnp.array) -> jnp.
     return softmax(q @ k.T / jnp.sqrt(q.shape[-1]) + mask) @ v
 
 
-def causal_self_attention(x: jnp.array,
-                          c_attn: jnp.array,
-                          c_proj: jnp.array):
+def causal_self_attention(x: jnp.array, c_attn: jnp.array, c_proj: jnp.array):
     x = linear(x, **c_attn)
 
     q, k, v = jnp.split(x, 3, axis=1)
@@ -35,13 +33,14 @@ def causal_self_attention(x: jnp.array,
     return linear(x, **c_proj)
 
 
-def multihead_attn(x: jnp.array, c_attn: jnp.array, c_proj: jnp.array, n_head: int) -> jnp.array:
+def multihead_attn(
+    x: jnp.array, c_attn: jnp.array, c_proj: jnp.array, n_head: int
+) -> jnp.array:
     x = linear(x, **c_attn)
     qkv = jnp.split(x, 3, axis=-1)
     qkv_heads = list(map(lambda x: jnp.split(x, n_head, axis=1), qkv))
-    causal_mask = (1-jnp.tri(x.shape[0]), dtype=x.dtype)
-    out_heads = [attention(q, k, v, causal_mask)
-                 for q, k, v in zip(*qkv_heads)]
+    causal_mask = (1 - jnp.tri(x.shape[0], dtype=x.dtype)) * -1e10
+    out_heads = [attention(q, k, v, causal_mask) for q, k, v in zip(*qkv_heads)]
     x = jnp.hstack(out_heads)
     return linear(x, **c_proj)
 
@@ -59,14 +58,7 @@ def transformer_block(x, mlp, attn, ln_1, ln_2, n_head):
     return x
 
 
-def gpt2(
-        inputs: List[int],
-        wte: jnp.array,
-        wpe,
-        blocks: Dict,
-        ln_f,
-        n_head: int
-):
+def gpt2(inputs: List[int], wte: jnp.array, wpe, blocks: Dict, ln_f, n_head: int):
     x = wte[inputs] + wpe(range(len(inputs)))
 
     for block in blocks:
@@ -76,12 +68,7 @@ def gpt2(
     return x @ wte.T
 
 
-def generate(
-        inputs: List[int],
-        eos_id: int,
-        max_seq_len: int
-) -> List[int]:
-
+def generate(inputs: List[int], eos_id: int, max_seq_len: int) -> List[int]:
     promt_len = len(inputs)
     while inputs[-1] != eos_id and len(inputs) < max_seq_len:
         output = gpt2(inputs)
@@ -90,24 +77,23 @@ def generate(
     return inputs[promt_len:]
 
 
-def main(prompt: str,
-         n_tokens_to_generate: int = 40,
-         model_size: str = '124M',
-         models_dir: str = 'models'):
+def main(
+    prompt: str,
+    n_tokens_to_generate: int = 40,
+    model_size: str = "124M",
+    models_dir: str = "models",
+):
     from utils import load_encoder_hparams_and_params
 
-    encoder, hparams, params = load_encoder_hparams_and_params(
-        model_size, models_dir)
+    encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
     input_ids = encoder.encode(prompt)
-    assert len(input_ids) + n_tokens_to_generate < hparams['n_ctx']
+    assert len(input_ids) + n_tokens_to_generate < hparams["n_ctx"]
 
-    output_ids = generate(
-        input_ids, params, hparams['n_head'], n_tokens_to_generate)
+    output_ids = generate(input_ids, params, hparams["n_head"], n_tokens_to_generate)
     output_text = encoder.decode(output_ids)
 
     return output_text
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     fire.Fire(main)