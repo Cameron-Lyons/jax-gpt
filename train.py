ed06c517b11586b6269aa780f5989577a02ca6c9
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-16 19:29:24 -0500

    i/o

--- train.py
+++ train.py
@@ -8,3 +8,13 @@ from contextlib import nullcontext
 
 import jax.numpy as jnp
 import jax
+from typing import Literal
+
+# I/O
+out_dir: str = "out"
+eval_interval: int = 2000
+log_interval: int = 1
+eval_iters: int = 200
+eval_only: bool = False  # if True, script exits right after the first eval
+always_save_checkpoint: bool = True  # if True, always save a checkpoint after each eval
+init_from: Literal["scratch", "resume", "gpt2"] = "scratch"
