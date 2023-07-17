434f0a28d71dfcbc3621d26c23714d0edcb7fa05
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-16 19:23:00 -0500

    dataclass

--- gpt2.py
+++ gpt2.py
@@ -3,6 +3,20 @@ from typing import List, Dict, Any, Literal
 import jax.numpy as jnp
 import jax
 from utils import load_encoder_hparams_and_params
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    """GPT-2 model configuration."""
+
+    block_size: int = 1024
+    vocab_size: int = 50304  # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 
 def lm_loss(params, inputs, n_head) -> jax.Array:
