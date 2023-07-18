5e560f39f27713007a93796419e13c31e4a48b15
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-18 10:20:20 -0500

    init model

--- train.py
+++ train.py
@@ -181,14 +181,18 @@ def estimate_loss() -> Dict[str, float]:
 
 
 def get_lr(it: int) -> float:
-    # 1) linear warmup for warmup_iters steps
     if it < warmup_iters:
         return learning_rate * it / warmup_iters
-    # 2) if it > lr_decay_iters, return min learning rate
     if it > lr_decay_iters:
         return min_lr
-    # 3) in between, use cosine decay down to min learning rate
     decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
     assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff ranges 0..1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
     return min_lr + coeff * (learning_rate - min_lr)
+
+
+X, Y = get_batch("train")
+t0 = time.time()
+local_iter_num: int = 0
+raw_model = model
+running_mfu: float = -1.0
