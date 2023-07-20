d36d876452ca7e1bb227d4e6c4a7f0649abb7650
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-20 07:58:53 -0500

    config

--- benchmark.py
+++ benchmark.py
@@ -7,3 +7,18 @@ import jax.numpy as jnp
 import time
 import torch
 from gpt2 import GPTConfig, gpt2
+
+batch_size: int = 12
+block_size: int = 1024
+bias: bool = False
+real_data: bool = True
+seed: int = 0
+device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+dtype = (
+    "bfloat16"
+    if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
+    else "float16"
+)  # 'float32' or 'bfloat16' or 'float16'
+compile: bool = True
+profile: bool = False
+exec(open("configurator.py").read())  # overrides from command line or config file
