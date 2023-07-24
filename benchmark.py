ca1a7f2a37c90b2ad646a5ac77957ae3d626e6e3
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-23 20:44:20 -0500

    real data for bench

--- benchmark.py
+++ benchmark.py
@@ -20,3 +20,33 @@ dtype: Literal["bfloat16", "float16", "float"] = "bfloat16"
 compile: bool = True
 profile: bool = False
 exec(open("configurator.py").read())  # overrides from command line or config file
+
+if real_data:
+    dataset = "openwebtext"
+    data_dir = os.path.join("data", dataset)
+    train_data = jnp.memmap(
+        os.path.join(data_dir, "train.bin"), dtype=jnp.uint16, mode="r"
+    )
+
+    def get_batch(split):
+        data = train_data  # note ignore split in benchmarking script
+        ix = torch.randint(len(data) - block_size, (batch_size,))
+        x = torch.stack(
+            [torch.from_numpy((data[i : i + block_size]).astype(jnp.int64)) for i in ix]
+        )
+        y = torch.stack(
+            [
+                torch.from_numpy((data[i + 1 : i + 1 + block_size]).astype(np.int64))
+                for i in ix
+            ]
+        )
+        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(
+            device, non_blocking=True
+        )
+        return x, y
+
+else:
+    # alternatively, if fixed data is desired to not care about data loading
+    x = torch.randint(50304, (batch_size, block_size), device=device)
+    y = torch.randint(50304, (batch_size, block_size), device=device)
+    get_batch = lambda split: (x, y)
