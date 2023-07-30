8cef1cc2400d2390c1278845ebf0c31e2cd465e9
Cameron Lyons <cameron.lyons2@gmail.com>
2023-07-29 21:26:56 -0500

    lint

--- train.py
+++ train.py
@@ -74,6 +74,7 @@ val_data: jax.Array = jnp.load(
 
 
 def get_batch(split):
+    """get batches for training"""
     data = train_data if split == "train" else val_data
     ix = jax.random.randint(random_key, (), 0, data.shape[0])
     x = jnp.stack([data[i:i + block_size].astype(jnp.int64) for i in ix], axis=-1)
