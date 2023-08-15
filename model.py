fa7bc87739fce35a0c50f2385bc3e7af558fd4d8
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-15 14:45:45 -0500

    gptconfig

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
-from flax import linen as nn
 from jax.scipy.special import logsumexp
+from flax import linen as nn
+from flax.struct import dataclass
 import math
 
 
@@ -84,6 +85,20 @@ class Block(nn.Module):
         x = x + self.mlpf(self.ln_2(x))
         return x
 
+
+@dataclass
+class GPTConfig:
+    model_type: str = "gpt"
+    n_layer: int = None
+    n_head: int = None
+    n_embd: int = None
+    vocab_size: int = None
+    block_size: int = None
+    embd_pdrop: float = 0.1
+    resid_pdrop: float = 0.1
+    attn_pdrop: float = 0.1
+
+
 class GPT(nn.Module):
     config: GPTConfig
 
@@ -93,24 +108,21 @@ class GPT(nn.Module):
         assert config.block_size is not None
 
         if config.model_type:
-        model_configs = {
-    # GPT-1
-    'openai-gpt':   dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
-
-    # GPT-2 configs
-    'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-    'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-    'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-    'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
-
-    # Gophers
-    'gopher-44m':   dict(n_layer=8, n_head=16, n_embd=512),
-
-    # Smaller custom models
-    'gpt-mini':     dict(n_layer=6, n_head=6, n_embd=192),
-    'gpt-micro':    dict(n_layer=4, n_head=4, n_embd=128),
-    'gpt-nano':     dict(n_layer=3, n_head=3, n_embd=48),
-}
+            model_configs = {
+                # GPT-1
+                "openai-gpt": dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
+                # GPT-2 configs
+                "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+                "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+                "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+                "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
+                # Gophers
+                "gopher-44m": dict(n_layer=8, n_head=16, n_embd=512),
+                # Smaller custom models
+                "gpt-mini": dict(n_layer=6, n_head=6, n_embd=192),
+                "gpt-micro": dict(n_layer=4, n_head=4, n_embd=128),
+                "gpt-nano": dict(n_layer=3, n_head=3, n_embd=48),
+            }
 
             config = GPTConfig(**model_configs[config.model_type])
 
@@ -119,5 +131,8 @@ class GPT(nn.Module):
         self.drop = nn.Dropout(rate=config.embd_pdrop)
         self.h = [Block(config) for _ in range(config.n_layer)]
         self.ln_f = nn.LayerNorm()
-        self.lm_head = nn.Dense(config.vocab_size, kernel_init=nn.initializers.xavier_uniform(), use_bias=False)
-
+        self.lm_head = nn.Dense(
+            config.vocab_size,
+            kernel_init=nn.initializers.xavier_uniform(),
+            use_bias=False,
+        )
