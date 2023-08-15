515075a20e6b07fd47d207d8e9af424270b7654f
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-15 14:43:37 -0500

    init model

--- model.py
+++ model.py
@@ -83,3 +83,41 @@ class Block(nn.Module):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlpf(self.ln_2(x))
         return x
+
+class GPT(nn.Module):
+    config: GPTConfig
+
+    def setup(self):
+        config = self.config
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+
+        if config.model_type:
+        model_configs = {
+    # GPT-1
+    'openai-gpt':   dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
+
+    # GPT-2 configs
+    'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+    'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+    'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+    'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+
+    # Gophers
+    'gopher-44m':   dict(n_layer=8, n_head=16, n_embd=512),
+
+    # Smaller custom models
+    'gpt-mini':     dict(n_layer=6, n_head=6, n_embd=192),
+    'gpt-micro':    dict(n_layer=4, n_head=4, n_embd=128),
+    'gpt-nano':     dict(n_layer=3, n_head=3, n_embd=48),
+}
+
+            config = GPTConfig(**model_configs[config.model_type])
+
+        self.wte = nn.Embed(vocab_size=config.vocab_size, features=config.n_embd)
+        self.wpe = nn.Embed(vocab_size=config.block_size, features=config.n_embd)
+        self.drop = nn.Dropout(rate=config.embd_pdrop)
+        self.h = [Block(config) for _ in range(config.n_layer)]
+        self.ln_f = nn.LayerNorm()
+        self.lm_head = nn.Dense(config.vocab_size, kernel_init=nn.initializers.xavier_uniform(), use_bias=False)
+
