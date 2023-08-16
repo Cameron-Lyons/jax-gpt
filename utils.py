35a55a4fc6263ac420b056adea865ab3d87d2346
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-16 16:58:49 -0500

    setup_logging

--- utils.py
+++ utils.py
@@ -92,3 +92,15 @@ def load_encoder_hparams_and_params(
     params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, hparams)
 
     return encoder, hparams, params
+
+def setup_logging(config):
+    """ monotonous bookkeeping """
+    work_dir = config.system.work_dir
+    # create the work directory if it doesn't already exist
+    os.makedirs(work_dir, exist_ok=True)
+    # log the args (if any)
+    with open(os.path.join(work_dir, 'args.txt'), 'w') as f:
+        f.write(' '.join(sys.argv))
+    # log the config itself
+    with open(os.path.join(work_dir, 'config.json'), 'w') as f:
+        f.write(json.dumps(config.to_dict(), indent=4))
